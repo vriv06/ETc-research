f923c12047e5d6b71480d7ee7b4bdcbc8fe44a10
Victor Rivero <85038276+vriv06@users.noreply.github.com>
2024-07-12 09:24:48 +0000

    day 192-193 data

--- btk_de_2_basil_2024/btkdebasil2024.py
+++ btk_de_2_basil_2024/btkdebasil2024.py
@@ -71,14 +71,17 @@ def run():
         Users can override for custom loading of irrigation data.
     """
     irr = fao.Irrigation(comment = '2024 basil')
-    irr.addevent(2024, 189, 3.284, 0.30)
-    irr.addevent(2024, 190, 4.975, 0.30)
+    irr.addevent(2024, 189, 2.800, 0.30)
+    irr.addevent(2024, 190, 3.284, 0.30)
+    irr.addevent(2024, 191, 4.975, 0.30)
+    irr.addevent(2024, 192, 5.317, 0.30)
+    irr.addevent(2024, 193, 4.200, 0.30)
     irr.savefile(os.path.join(module_dir,'btkdebasil2024.irr'))
     irr.loadfile(os.path.join(module_dir,'btkdebasil2024.irr'))
    
 
     #Run the model
-    mdl = fao.Model('2024-158','2024-191', par, wth, irr=irr, aq_Ks=True,
+    mdl = fao.Model('2024-158','2024-193', par, wth, irr=irr, aq_Ks=True,
                     comment = 'btk 2024 basil trial')
     mdl.run()
     print(mdl)
